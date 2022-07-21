7cadbe20a82a6f00327231e2908191075d8e69f1
maelstrom <cephalopodmaster@protonmail.com>
2022-07-21 16:19:25 -0400

    Added data path

--- local_paths.py
+++ local_paths.py
@@ -7,4 +7,8 @@ with open('paths.json', 'w') as pathfile:
     codepath = os.path.dirname(os.path.realpath(__file__))
     codepath = os.path.dirname(codepath+'..')
     paths['codepath'] = codepath
+
+    datapath = os.path.dirname(codepath+'..')
+    datapath = os.path.dirname(str(datapath+'\\data\\'))
+    paths['datapath'] = datapath
     json.dump(paths, pathfile)
