24ccee501614217443926cadac987af31a5f25ae
maelstrom <cephalopodmaster@protonmail.com>
2022-11-01 15:14:44 -0400

    Now replaces \\s with / so that both windows and unix can have /s

--- local_paths.py
+++ local_paths.py
@@ -6,9 +6,11 @@ with open('paths.json', 'w') as pathfile:
     paths = {}
     codepath = os.path.dirname(os.path.realpath(__file__))
     codepath = os.path.dirname(codepath+'..')
+    codepath = codepath.replace('\\', '/')
     paths['codepath'] = codepath
 
     datapath = os.path.dirname(codepath+'..')
-    datapath = os.path.dirname(str(datapath+'\\data\\'))
+    datapath = os.path.dirname(str(datapath+'/data/'))
+    datapath = datapath.replace('\\', '/')
     paths['datapath'] = datapath
     json.dump(paths, pathfile)
